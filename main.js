ee384879f66e0d319addc927a51f849654d1db82
colecrist <colepcrist@gmail.com>
2025-03-11 12:56:15 -0700

    Updated scripts

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
 console.log("Webflow External Script Loaded!");
-
-// Example: GSAP Scroll Animation
-gsap.from(".your-element", {
-    opacity: 0,
-    y: 50,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".your-element",
-        start: "top 80%",
-        scrub: true
-    }
-});
